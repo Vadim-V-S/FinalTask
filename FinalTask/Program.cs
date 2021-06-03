3f80089e0f1759241dc7f1f3d2eed636a09d9084
Vadim-V-S <vaddimsav@gmail.com>
2021-06-03 16:13:21 +0500

    Добавьте файлы проекта.

---
+++ FinalTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static Student[] students;
+        static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        static string TargetsFolder = string.Format(@"{0}\Student",desktopPath);
+
+        static void Main(string[] args)
+        {
+            string sourcePath = string.Format(@"{0}\Students.dat", desktopPath);
+
+            using (var fs = new FileStream(sourcePath, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                students = (Student[])formatter.Deserialize(fs);
+
+                if (!Directory.Exists(TargetsFolder))
+                {
+                    Directory.CreateDirectory(TargetsFolder);
+                }
+
+                int i = 0;
+                foreach (Student st in students)
+                {
+                    string targetPath = string.Format($"{TargetsFolder}\\{students[i].Group}.txt");
+                    if (!File.Exists(targetPath))
+                    {
+                        using StreamWriter sw = File.CreateText(targetPath);
+                    }
+                    using (StreamWriter sw = File.AppendText(targetPath))
+                    {
+                        sw.WriteLine(string.Format($"{students[i].Name}, {students[i].DateOfBirth}"));
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
